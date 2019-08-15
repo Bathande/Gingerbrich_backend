a6939b3229747d0105237e1adafeb47a5a41a41d
Bathande <bndaba@retrorabbit.co.za>
2019-08-15 15:48:11 +0200

    commit

---
+++ Gingerbrich_backend/Gingerbrich_backend/Models/ModelContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gingerbrich_backend.Models;
+namespace Gingerbrich_backend.Models
+{
+    public class ModelContext:DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options): base(options) { }
+        public DbSet<Product> Articles { get; set; }
+    }
+}
---
+++ Gingerbrich_backend/Gingerbrich_backend/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Gingerbrich_backend.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Type {get;set;}
+    }
+}
