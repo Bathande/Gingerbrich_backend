6a88f097c16b2ce2017046ab67c2ecc61294f04f
Bathande <bndaba@retrorabbit.co.za>
2019-09-09 21:35:18 +0200

    commit

---
+++ Gingerbrich_backend/Models/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gingerbrich_backend.Models
+{
+    public class ModelContext: DbContext
+    {
+        public ModelContext(DbContextOptions<ModelContext> options) : base(options)
+        {
+
+
+        }
+        public virtual DbSet<Person> Person { get; set; }
+    }
+}
